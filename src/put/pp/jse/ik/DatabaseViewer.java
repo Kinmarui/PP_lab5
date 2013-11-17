377752b835dd0f02038e91188bf42eac43692da6
Ireneusz Kozlowski <ireneusz.kozlowskii@gmail.com>
2013-11-17 17:29:09 +0100

    Added pane with borderLayout
    Including
    Label on top,
    2x Button on sides,
    TextArea on center,
    TextField on bottom

--- src/put/pp/jse/ik/DatabaseViewer.java
+++ src/put/pp/jse/ik/DatabaseViewer.java
@@ -1,6 +1,7 @@
 package put.pp.jse.ik;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,12 +11,38 @@ import javax.swing.*;
  * Platformy Programowania Lab5 DatabseViewer
  */
 public class DatabaseViewer extends JFrame {
-      public static void main(String args[]) {
-          java.awt.EventQueue.invokeLater(new Runnable() {
-              @Override
-              public void run() {
-                  new DatabaseViewer().setVisible(true);
-              }
-          });
-      }
+
+
+    public static void main(String args[]) {
+      java.awt.EventQueue.invokeLater(new Runnable() {
+          @Override
+          public void run() {
+              initComponents();
+              new DatabaseViewer().setVisible(true);
+          }
+      });
+    }
+
+    public static void  initComponents(){
+        JFrame frame;
+        Container pane;
+
+        frame = new JFrame("Database Viewer");
+        pane = frame.getContentPane();
+
+        JLabel mousePos = new JLabel("Pozycja myszy");
+        JButton clean = new JButton("Wyczyść");
+        JTextArea output = new JTextArea("Output");
+        JButton execute = new JButton("Wykonaj");
+        JTextField input = new JTextField("Enter connamds here");
+
+        pane.add(mousePos,BorderLayout.PAGE_START);
+        pane.add(clean, BorderLayout.LINE_START);
+        pane.add(output, BorderLayout.CENTER);
+        pane.add(execute, BorderLayout.LINE_END);
+        pane.add(input, BorderLayout.PAGE_END);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
